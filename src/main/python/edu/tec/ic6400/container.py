24d8d3373153213e3db97890ed1b2d386f72ad5c
Adriel <adriel369@gmail.com>
2021-06-02 21:43:39 -0600

    [+] Se crean los rangos aleatorios para hacer el calculo -p

--- src/main/python/edu/tec/ic6400/container.py
+++ src/main/python/edu/tec/ic6400/container.py
@@ -2,6 +2,7 @@ import sys
 from sys import path
 path.append('src.main.python.edu.tec.ic6400')
 import knapsack
+import random
 
 
 # total arguments
@@ -9,13 +10,42 @@ mode=sys.argv[2]
 
 if(mode=="-p"):
     print("Se quiere hacer del modo aleatorio")
+
     algorithm=sys.argv[1]
     weight=sys.argv[3]
     number_of_elements=sys.argv[4]
     weight_range=sys.argv[5]
-    range_Benefits=sys.argv[6]
+    benefits_range=sys.argv[6]
+
+    #Se hace un split para separar los numeros y hacer el rango
+    partitioned_string = weight_range.partition('-')
+    weight_range_1= partitioned_string[0]
+    weight_range_2 = partitioned_string[2]
+    partitioned_string = benefits_range.partition('-')
+    benefits_range_1= partitioned_string[0]
+    benefits_range_2 = partitioned_string[2]
     iterations=sys.argv[7]
-    print(algorithm,weight,number_of_elements,weight_range,range_Benefits,iterations)
+
+    #se junta los numeros y se guarda el rango
+    benefits_range=(range(int(benefits_range_1),int(benefits_range_2)))
+    weight_range=(range(int(weight_range_1),int(weight_range_2)))
+    benefits_range_list=[]
+    weight_range_list=[]
+
+    #se almacena la lista de numeros en listas
+    for i in benefits_range:
+        benefits_range_list.append(i)
+
+    for i in weight_range:
+        weight_range_list.append(i)
+
+    #se hace un random(desordenan) los numeros de las listas para efectuar el aleatorio
+    random.shuffle(benefits_range_list)
+    random.shuffle(weight_range_list)
+
+    print(benefits_range_list)
+    print(weight_range_list)
+    print(algorithm,weight,number_of_elements,weight_range,benefits_range,iterations)
 
 elif(mode=="-a"):
     print("Se quiere hacer del modo manual")
